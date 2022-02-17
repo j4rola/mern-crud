8d24bf6db6c8e33721915a01127a74dc542a6439
Joe Froula <joefroula@Joes-MacBook-Air.local>
2022-02-17 14:53:46 -0800

    adds react router and create user form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,70 @@
+import { useState, useEffect } from 'react'
+import { FaUser } from 'react-icons/fa'
+
 function Register() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+
+    const { name, email, password, password2 } = formData
+
+    const onChange = (e) => {
+        setFormData(
+            (prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value
+            }) 
+        )
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <div>Register</div>
+    <>
+        <section className="heading">
+            <h1>
+                <FaUser/> Register
+            </h1>
+            <p>Please create an account</p>
+        </section>
+        <section className="form">
+            <div className="form-group">
+                <form >
+                    <input type="text" className="form-control" id="name" 
+                    name="name" value={name} placeholder="Enter your name" onChange={onChange}/>
+                </form>
+            </div>
+            <div className="form-group">
+                <form >
+                    <input type="email" className="form-control" id="email" 
+                    name="email" value={email} placeholder="Enter your email" onChange={onChange}/>
+                </form>
+            </div>
+            <div className="form-group">
+                <form >
+                    <input type="password" className="form-control" id="password" 
+                    name="password" value={password} placeholder="Enter a password" onChange={onChange}/>
+                </form>
+            </div>
+            <div className="form-group">
+                <form >
+                    <input type="password" className="form-control" id="password2" 
+                    name="password2" value={password2} placeholder="Confirm your password" onChange={onChange}/>
+                </form>
+            </div>
+            <div className="form-group">
+                <button type="submit" className="btn btn-block">
+                    Submit
+                </button>
+            </div>
+        </section>
+    </>
   )
 }
 
